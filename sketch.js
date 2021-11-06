ca58b6194c2e26bab6bcf9681dc256066f31196b
JkvWolf <93736776+JkvWolf@users.noreply.github.com>
2021-11-06 16:52:38 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var paddle1;
+var paddle2;
+var ball;
+var score1;
+var score2;
+function preload(){
+ 
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+ 
+ 
+ 
+}
+
+function draw() {
+  background(180);
+  
+  drawSprites();
+}
+
