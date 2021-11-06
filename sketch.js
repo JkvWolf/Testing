045a61c391d5e79d6b86344cb7c8586e5ff71535
JkvWolf <93736776+JkvWolf@users.noreply.github.com>
2021-11-06 16:43:15 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var test;
+
+function preload(){
+ 
+}
+
+function setup() {
+  createCanvas(400, 400);
+ 
+ test = createSprite(200, 200, 100, 100);
+ 
+}
+
+function draw() {
+  background(180);
+  
+  drawSprites();
+}
+
